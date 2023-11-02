08dfeef37de72214d2f14fb07ca931a7fef73e31
JamesMille02 <144379986+JamesMille02@users.noreply.github.com>
2023-11-02 14:32:51 -0400

    Update linear_regression.py
    Added function, library and doc strings for file

---
+++ Machine Learning Models/linear_regression.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+def linear_regression_model(csv_file):
+    """Predicts the prices of house utilizing the sklearns Linear Regression
+    machine learning algorithm by splitting into test and train values and 
+    getting the percent differnce in predicted and actual values.
+
+    Args:
+        csv_file(str): the file that contains the housing data set in csv file
+            format.
+    """
