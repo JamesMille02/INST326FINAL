b42f5b29c798d6ff52955bd060a513a723caf006
JamesMille02 <144379986+JamesMille02@users.noreply.github.com>
2023-11-21 18:43:51 -0500

    creates the bed prediction model

--- data set prep/beds_prediction.py
+++ data set prep/beds_prediction.py
@@ -14,43 +14,50 @@ def compute_null_bed(csv_file):
         The altered dataset with imputed 'bed' values.
     """
     try:
-        # Load the CSV file into a Pandas DataFrame
+        #loads the csv file
         df = pd.read_csv(csv_file)
 
-        # Define the features (predictors) and the target variable
+        #define the features
         categorical_features = ['PROPERTY TYPE', 'CITY', 'ZIP OR POSTAL CODE']
         numeric_features = ['PRICE', 'LATITUDE', 'LONGITUDE']
 
-        # Select relevant columns for modeling
+        #select relevant columns for modeling
         selected_columns = ['BEDS'] + categorical_features + numeric_features
+        #sets the columns to the columns in the dataframe
         df_selected = df[selected_columns]
 
-        # Separate data into features (X) and target variable (y)
+        #separate data into features (X) and target variable (y)
         X = df_selected.drop('BEDS', axis=1)
         y = df_selected['BEDS']
 
-        # Handle missing values in the target variable 'y'
+        #handle missing values in the target variable 'y'
         imputer = SimpleImputer(strategy='mean')
+        #creates imputer object to fill missing values
         y_imputed = imputer.fit_transform(y.values.reshape(-1, 1))
+        #creates flattened version of y_imputed array
         y = pd.Series(y_imputed.flatten(), name='BEDS')
 
-        # Handle categorical features using one-hot encoding
+        #handle categorical features using one-hot encoding
         X_encoded = pd.get_dummies(X, columns=categorical_features)
 
-        # Split the data into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.2, random_state=42)
+        #split the data into training and testing sets
+        X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, 
+                                                            test_size=0.2, 
+                                                            random_state=42)
 
-        # Train a Linear Regression model
+        #train a Linear Regression model
         model = LinearRegression()
+        #fits the model with the train data
         model.fit(X_train, y_train)
 
-        # Predict 'bed' values for rows with missing values
+        #predict 'BEDS' values for rows with missing values
         X_missing_bed = X_encoded[df['BEDS'].isnull()]
         predicted_bed_values = model.predict(X_missing_bed)
 
-        # Update the 'bed' column with the predicted values
+        #update the 'BEDS' column with the predicted values
         df.loc[df['BEDS'].isnull(), 'BEDS'] = predicted_bed_values
 
+        #saves it to the csv
         df.to_csv(csv_file, index=False)
 
     except Exception as e:
@@ -58,7 +65,7 @@ def compute_null_bed(csv_file):
         return None
 
 
-# Example usage:
+#example usage:
 csv_file_path = "feature_prediction.csv"
 compute_null_bed(csv_file_path)
 
