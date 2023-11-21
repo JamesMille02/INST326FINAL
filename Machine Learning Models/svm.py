4ea0053460c9ab7cff3bd9909753d826e086ab4b
JamesMille02 <144379986+JamesMille02@users.noreply.github.com>
2023-11-21 18:41:32 -0500

    created the svm model

--- Machine Learning Models/svm.py
+++ Machine Learning Models/svm.py
@@ -1,18 +1,46 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn import svm
+from sklearn.svm import SVR
 from sklearn.metrics import mean_absolute_error
 
-def linear_regression_model(csv_file):
-    """Predicts the prices of house utilizing the sklearns support vector
-    machines machine learning algorithm by splitting into test and train values 
-    and getting the differnce in predicted and actual values.
+def support_vector_regression_model(csv_file):
+    """Predicts the prices of houses using scikit-learn's Support Vector Regression 
+    machine learning algorithm by splitting into test and train values, and 
+    getting the mean absolute error (MAE) in predicted and actual values.
 
     Args:
-        csv_file(str): the file that contains the housing data set in csv file
+        csv_file (str): The file that contains the housing dataset in CSV file
             format.
-    
+
     Returns:
-        A variable which contains the mean absolute error for 
-            comparing the models accuracy.   
+        float: The mean absolute error for comparing the model's accuracy.
     """
+    #loads the dataset into a df
+    df = pd.read_csv(csv_file)
+
+    #specifies the features (X) and target variable (y)
+    features = df.drop('PRICE', axis=1)
+    target = df['PRICE']
+
+    # Split the dataset into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(features, target, 
+                                                        test_size=0.2, 
+                                                        random_state=42)
+
+    #creates and train the Support Vector Regression (SVR) model
+    model = SVR()
+    #fits the train data to the model
+    model.fit(X_train, y_train)
+
+    #makes the predictions on the test set
+    y_pred = model.predict(X_test)
+
+    #calculates the mean absolute error
+    mean_absolute_error_value = mean_absolute_error(y_test, y_pred)
+
+    return mean_absolute_error_value
+
+# Example usage:  
+mean_absolute_error_value = support_vector_regression_model(
+    "feature_prediction.csv")
+print(f"Mean Absolute Error: {mean_absolute_error_value}")
