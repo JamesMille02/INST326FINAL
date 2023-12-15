ac1dd6d11d20b8f6c567205e3447f876e4928ac1
JamesMille02 <144379986+JamesMille02@users.noreply.github.com>
2023-12-15 00:11:15 -0500

    cleanedthe data cleaning tests page

--- unitTests/data_cleaning_tests.py
+++ unitTests/data_cleaning_tests.py
@@ -62,7 +62,6 @@ class TestCSVFunctions(unittest.TestCase):
             file_path = os.path.join(self.temp_dir, file_name)
             #removes the file through the path
             os.remove(file_path)
-        
         #removes the temporary directory after testing
         os.rmdir(self.temp_dir)
 
@@ -99,7 +98,6 @@ class TestCSVFunctions(unittest.TestCase):
         test_csv_data = {"A": [1, 2, 3], "B": [4, 5, 6]}
         #adds the data to the csv  file
         test_csv = pd.DataFrame(test_csv_data)
-
         #determins the column to drop
         columns_dropped = ["B"]
         #calls the drop_unwanted_columns method to test it 
@@ -129,6 +127,7 @@ class TestCSVFunctions(unittest.TestCase):
         expected_output = f"Null value counts per column:"
         "\nA    1\nB    1\ndtype: int64"
         is_null_output = out.getvalue().strip()
+
         self.assertIn(expected_output, is_null_output)
 
     def test_is_null_without_null_values(self):
@@ -142,17 +141,16 @@ class TestCSVFunctions(unittest.TestCase):
         pd.DataFrame(test_csv).to_csv(os.path.join(self.temp_dir, 
                                                         "tests_no_null.csv"), 
                                                         index=False)
-
         #saves the file path
         file_output = os.path.join(self.temp_dir, "tests_no_null.csv")
         #captures the output of the is_null method with the file path to
         #the temp directory
         with captured_output() as (out, err):
             is_null(file_output)
-
         # Check if the expected message is present in the printed output
         expected_output = "The CSV file does not contain any null values."
         no_null_output= out.getvalue().strip()
+
         self.assertIn(expected_output, no_null_output)
 
     def test_property_type_values(self):
@@ -175,19 +173,19 @@ class TestCSVFunctions(unittest.TestCase):
         #calls the property_type_values and saves the output
         with captured_output() as (out, err):
             property_type_values(file_output)
-
         #checks to see if the output is expected
         expected_output = "House        2\nApartment    2\nCondo        1"
         property_type_values_output = out.getvalue().strip()
+
         self.assertIn(expected_output, property_type_values_output)
 
     def test_property_type_reclassification(self):
         """makes the property_type_reclassification method is properly changing
         property types to the correct numeric values.
         """
+
         #creates temp csv
         test_csv = tempfile.NamedTemporaryFile(delete=False, suffix=".csv")
-
         #created data
         data = {
             'PROPERTY TYPE': ['Single Family Residential', 'Townhouse', 
@@ -199,19 +197,15 @@ class TestCSVFunctions(unittest.TestCase):
         test_data = pd.DataFrame(data)
         #saves the data frame to the temp file
         test_data.to_csv(test_csv.name, index=False)
-
         #calls the property_type_reclassification on the temp file
         property_type_reclassification(test_csv.name)
-
         #reads the contents
         modified_data = pd.read_csv(test_csv.name)
-
         #expected mapping for the data
         expected_mapping = {
             'Single Family Residential': 1,
             'Townhouse': 2
         }
-
         #assigns the property types values for the expected results
         expected_values = test_data['PROPERTY TYPE'].map(expected_mapping)
 
@@ -230,22 +224,22 @@ class TestCSVFunctions(unittest.TestCase):
         pd.DataFrame(test_csv).to_csv(os.path.join(self.temp_dir, 
                                                         "testing_cities.csv"), 
                                                         index=False)
-
         #saves the file path
         file_output = os.path.join(self.temp_dir, "testing_cities.csv")
         #saves the output of find_unique_city
         with captured_output() as (out, err):
             find_unique_cities(file_output)
-
         #checks if the expected message is present in the printed output
         expected_output = "['City1', 'City2', 'City3']"
         find_unique_cities_output = out.getvalue().strip()
+
         self.assertIn(expected_output, find_unique_cities_output)
 
     def test_property_type_drop_rows(self):
         """tests if the right columns are able to be dropped when using the 
         property_type_drop_rows method.
         """
+
         #created test data
         test_csv = {"PROPERTY TYPE": ["House", "Condo", 
                                            "Single Family Residential", 
@@ -260,13 +254,12 @@ class TestCSVFunctions(unittest.TestCase):
         file_input = os.path.join(self.temp_dir, "testing_property_type_drop.csv")
         #calls the property_type_drop_rows method
         property_type_drop_rows(file_input)
-
         #reads the csv file
         modified_df = pd.read_csv(file_input)
-
         #assigns the values which are expected
         property_type_drop_rows_output = ["Single Family Residential", "Townhouse"]
         #sees if only the values in expected_values are present
+
         self.assertTrue(all(value in modified_df['PROPERTY TYPE'].values for 
                             value in property_type_drop_rows_output))
         #sees if it is the correct length
@@ -290,13 +283,12 @@ class TestCSVFunctions(unittest.TestCase):
                                   "test_city_reclass.csv")
         #calls the city classification method on the input file
         city_reclassification(file_path)
-
         #read the modified file and saves it
         city_reclassification_output = pd.read_csv(file_path)
-
         #defines the expected values
         expected_indices = [0, 1, 0, 2, 1]
         #checks to see if the indexes assigned are correct
+
         self.assertTrue(all(index == expected_index for index, expected_index 
                             in zip(city_reclassification_output['CITY'], 
                                    expected_indices)))
@@ -307,6 +299,7 @@ class TestCSVFunctions(unittest.TestCase):
         column types. This is important bc even if the values are numeric 
         the column types needs to be int or float or the model throughs errors.
         """
+
         #creates test  data
         test_csv = {"Column1": [3, 4, 5], "Column2": [4.0, 5.0, 6.0]}
         #saves the data to a file
@@ -317,19 +310,18 @@ class TestCSVFunctions(unittest.TestCase):
         #saves the path to the file in the temp directory
         file_path = os.path.join(self.temp_dir, 
                                   "test_column_class_check.csv")
-    
+        
         #opens the file
         with open(os.path.join(self.temp_dir, "test.txt"), 'w') as f:
             #saves the output from the method
             with redirect_stdout(f):
                 non_numeric_columns = column_classification_check(file_path)
-
         #reads the file changes
         with open(os.path.join(self.temp_dir, "test.txt"), 'r') as f:
             column_classification_output = f.read()
-
         #assigns the expect value to the expected values
         expected_output = "All columns have numeric types (int or float)."
+
         #tests if the values are the same and if the non_numeric_columns list
         #is empty
         self.assertIn(expected_output, column_classification_output)
@@ -345,25 +337,19 @@ class TestCSVFunctions(unittest.TestCase):
             'PRICE': [200000, 300000, 250000],
             'SQUARE FEET': [1500, 1800, 2000],
         })
-
         #saves temp data in a csv
         test_data.to_csv(os.path.join(self.temp_dir, 
                                       'test_calc_price_per_sqft.csv'), 
                                       index=False)
-
         #gets the file path
         file_path = os.path.join(self.temp_dir, 'test_calc_price_per_sqft.csv')
-
         #calls the cal_price_per_sqft function on the temp file
         calc_price_per_sqft(file_path)
-
         #reads the temp  file
         calc_price_per_sqft_output = pd.read_csv(file_path)
-
         #calculates the price per square foot
         expected_values = test_data['PRICE'] / test_data['SQUARE FEET']
         expected_values.fillna(0, inplace=True)
-
         #sets the name of the expected series to match the modified series 
         expected_values.name = '$/SQUARE FEET'
 
@@ -381,20 +367,15 @@ class TestCSVFunctions(unittest.TestCase):
             'SQUARE FEET': [1500, 1800, 2000],
             'LOT SIZE': [3000, None, 2500],
         })
-
         #saves data in csv
         lot_size_data.to_csv(os.path.join(self.temp_dir, 
                                       'test_lot_size_prep.csv'), index=False)
-
         #saves file path of the test CSV
         file_path = os.path.join(self.temp_dir, 'test_lot_size_prep.csv')
-
         #calls the lot_size_prep function on the temp file
         lot_size_prep(file_path)
-
         #reads the data in the CSV
         calc_price_per_sqft_file = pd.read_csv(file_path)
-
         #expected data with null lot size replaced by square footage
         expected_data = pd.DataFrame({
             'SQUARE FEET': [1500, 1800, 2000],
@@ -417,19 +398,17 @@ class TestCSVFunctions(unittest.TestCase):
         pd.DataFrame(test_csv).to_csv(os.path.join(self.temp_dir,
                                                          "test_hoa_prep.csv"), 
                                                          index=False)
-
         #saves the file path
         file_path = os.path.join(self.temp_dir, "test_hoa_prep.csv")
         #calls the hoa prep method
         hoa_prep(file_path)
-
         #reads the file and saves it 
         hoa_prep_result = pd.read_csv(file_path)
-
         #saves the expected values 
         expected_values = [100, 0, 150]
         #checks to see if the expected values are the same as what is in the
         #file
+
         self.assertListEqual(list(hoa_prep_result['HOA/MONTH']), 
                              expected_values)
 
@@ -437,7 +416,6 @@ class TestCSVFunctions(unittest.TestCase):
         """tests to determine if the correct rows are dropped during the 
         drop_null_rows method
         """
-
         #creates test data
         test_csv = {"BATHS": [2, None, 3, 1],
                           "SQUARE FEET": [1500, 1800, None, 2000], 
@@ -446,13 +424,10 @@ class TestCSVFunctions(unittest.TestCase):
         test_path = os.path.join(self.temp_dir, "test_drop_null_rows.csv")
         #creates the file with the temp data
         pd.DataFrame(test_csv).to_csv(test_path, index=False)
-
         #calls the drop_null_method on the created temp file
         drop_null_rows(test_path)
-
         #reads the file
         null_output = pd.read_csv(test_path)
-
         #assigns the expected result
         expected_data = {"BATHS": [2.0], "SQUARE FEET": [1500.0], 
                          "LOT SIZE": [3000.0]}
